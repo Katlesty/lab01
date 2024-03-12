ee99df819303a772f7531d54f9b6cd98d60816c6
Yahaira Harumi Chavez Vasquez <130882001+Katlesty@users.noreply.github.com>
2024-03-12 07:50:28 -0500

    pago

--- pago/urls.py
+++ pago/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:p>/<int:h>/',views.pago,name='pago')
 ]
--- pago/views.py
+++ pago/views.py
@@ -3,4 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+def index(request):
+    return HttpResponse("Bienvenido")
 
+def pago(request,p,h):
+    if h<=40:
+        res = p*h
+        return HttpResponse("El pago de la semana es: %s" % res)
+    else:
+        res = 2*(p*h)
+        return HttpResponse("El pago de la semana es %s" % res)
